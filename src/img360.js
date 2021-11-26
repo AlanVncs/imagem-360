f5e28b9a54d284235fa48767a32bfda9ad459df5
Lucas Rocha <lucas@agenciaeplus.com.br>
2021-11-26 02:43:21 -0300

    style

--- src/img360.js
+++ src/img360.js
@@ -7,18 +7,45 @@ var Img360 = /** @class */ (function () {
         this.autoPlayTask = 0;
         this.autoPlayingActive = false;
         this.$mainContainer = options.$mainContainer;
+        this.$thumbContainer = options.$thumbContainer;
         this.imageArray = options.imageArray;
         this.autoplaySpeed = options.autoplaySpeed || 200;
+        this.$buttonPrev = options.$buttonPrev;
+        this.$buttonNext = options.$buttonNext;
+        this.$buttonAutoplay = options.$buttonAutoplay;
         this.imageArray.forEach(function (image, index) {
-            var $img = document.createElement('img');
+            var $img = document.createElement("img");
             _this.$imgArray.push($img);
             $img.src = image;
             if (index != 0) {
-                $img.style.display = 'none';
+                $img.style.display = "none";
             }
             _this.$mainContainer.appendChild($img);
         });
+        var $img = document.createElement("img");
+        $img.src = this.imageArray[0];
+        this.$thumbContainer.appendChild($img);
+        this.setButtonsEnvents();
     }
+    Img360.prototype.setButtonsEnvents = function () {
+        var _this = this;
+        this.$buttonPrev.addEventListener("click", function () {
+            _this.prevImage();
+        });
+        this.$buttonNext.addEventListener("click", function () {
+            _this.nextImage();
+        });
+        this.$buttonAutoplay.addEventListener("click", function () {
+            if (_this.isAutoPdddlaying()) {
+                _this.stopAutoplay();
+                _this.$buttonAutoplay.classList.remove("playing");
+            }
+            else {
+                _this.startAutoplay();
+                _this.$buttonAutoplay.classList.add("playing");
+            }
+        });
+    };
     Img360.prototype.isAutoPdddlaying = function () {
         return this.autoPlayingActive;
     };
@@ -36,24 +63,24 @@ var Img360 = /** @class */ (function () {
         }
     };
     Img360.prototype.prevImage = function () {
-        this.$imgArray[this.index].style.display = 'none';
+        this.$imgArray[this.index].style.display = "none";
         if (this.index == 0) {
             this.index = this.imageArray.length - 1;
         }
         else {
             this.index -= 1;
         }
-        this.$imgArray[this.index].style.display = 'block';
+        this.$imgArray[this.index].style.display = "block";
     };
     Img360.prototype.nextImage = function () {
-        this.$imgArray[this.index].style.display = 'none';
+        this.$imgArray[this.index].style.display = "none";
         if (this.index == this.imageArray.length - 1) {
             this.index = 0;
         }
         else {
             this.index += 1;
         }
-        this.$imgArray[this.index].style.display = 'block';
+        this.$imgArray[this.index].style.display = "block";
     };
     return Img360;
 }());
--- src/img360.ts
+++ src/img360.ts
@@ -1,69 +1,103 @@
 interface Img360Props {
-	$mainContainer: Element
-	imageArray: string[]
-	autoplaySpeed: number
+  $mainContainer: Element;
+  imageArray: string[];
+  autoplaySpeed: number;
+  $buttonPrev: HTMLButtonElement;
+  $buttonNext: HTMLButtonElement;
+  $buttonAutoplay: HTMLButtonElement;
+	$thumbContainer: Element
 }
 
 class Img360 {
-	$mainContainer: Element
-	imageArray: string[]
-	autoplaySpeed: number
-	private index: number = 0
-	private $imgArray: HTMLImageElement[] = []
-	private autoPlayTask: number = 0
-	private autoPlayingActive: boolean = false
+  $mainContainer: Element;
+	$thumbContainer: Element;
+  imageArray: string[];
+  autoplaySpeed: number;
+  $buttonPrev: HTMLButtonElement;
+  $buttonNext: HTMLButtonElement;
+  $buttonAutoplay: HTMLButtonElement;
+  private index: number = 0;
+  private $imgArray: HTMLImageElement[] = [];
+  private autoPlayTask: number = 0;
+  private autoPlayingActive: boolean = false;
 
-	constructor(options: Img360Props) {
-		this.$mainContainer = options.$mainContainer
-		this.imageArray = options.imageArray
-		this.autoplaySpeed = options.autoplaySpeed || 200
+  constructor(options: Img360Props) {
+    this.$mainContainer = options.$mainContainer;
+		this.$thumbContainer = options.$thumbContainer
+    this.imageArray = options.imageArray;
+    this.autoplaySpeed = options.autoplaySpeed || 200;
+    this.$buttonPrev = options.$buttonPrev;
+    this.$buttonNext = options.$buttonNext;
+    this.$buttonAutoplay = options.$buttonAutoplay;
 
-		this.imageArray.forEach((image, index) => {
-			const $img = document.createElement('img')
-			this.$imgArray.push($img)
-			$img.src = image
-			if (index != 0) {
-				$img.style.display = 'none'
-			}
-			this.$mainContainer.appendChild($img)
-		})
-	}
+    this.imageArray.forEach((image, index) => {
+      const $img = document.createElement("img");
+      this.$imgArray.push($img);
+      $img.src = image;
+      if (index != 0) {
+        $img.style.display = "none";
+      }
+      this.$mainContainer.appendChild($img);
+    });
+		const $img = document.createElement("img");
+		$img.src = this.imageArray[0];
+		this.$thumbContainer.appendChild($img)
+		this.setButtonsEnvents()
+  }
 
-	isAutoPdddlaying() {
-		return this.autoPlayingActive
-	}
+  setButtonsEnvents() {
+    this.$buttonPrev.addEventListener("click", () => {
+      this.prevImage();
+    });
+    this.$buttonNext.addEventListener("click", () => {
+      this.nextImage();
+    });
+    this.$buttonAutoplay.addEventListener("click", () => {
+      if (this.isAutoPdddlaying()) {
+        this.stopAutoplay();
+        this.$buttonAutoplay.classList.remove("playing");
+      } else {
+				this.startAutoplay();
+        this.$buttonAutoplay.classList.add("playing");
+      }
+    });
+  }
 
-	startAutoplay() {
-		this.autoPlayTask = setInterval(() => {
-			this.nextImage()
-		}, this.autoplaySpeed)
-		this.autoPlayingActive = true
-	}
+  isAutoPdddlaying() {
+    return this.autoPlayingActive;
+  }
 
-	stopAutoplay() {
-		if (this.autoPlayTask != 0) {
-			clearInterval(this.autoPlayTask)
-			this.autoPlayingActive = false
-		}
-	}
+  startAutoplay() {
+    this.autoPlayTask = setInterval(() => {
+      this.nextImage();
+    }, this.autoplaySpeed);
+    this.autoPlayingActive = true;
+  }
 
-	prevImage() {
-		this.$imgArray[this.index].style.display = 'none'
-		if (this.index == 0) {
-			this.index = this.imageArray.length - 1
-		} else {
-			this.index -= 1
-		}
-		this.$imgArray[this.index].style.display = 'block'
-	}
+  stopAutoplay() {
+    if (this.autoPlayTask != 0) {
+      clearInterval(this.autoPlayTask);
+      this.autoPlayingActive = false;
+    }
+  }
 
-	nextImage() {
-		this.$imgArray[this.index].style.display = 'none'
-		if (this.index == this.imageArray.length - 1) {
-			this.index = 0
-		} else {
-			this.index += 1
-		}
-		this.$imgArray[this.index].style.display = 'block'
-	}
+  prevImage() {
+    this.$imgArray[this.index].style.display = "none";
+    if (this.index == 0) {
+      this.index = this.imageArray.length - 1;
+    } else {
+      this.index -= 1;
+    }
+    this.$imgArray[this.index].style.display = "block";
+  }
+
+  nextImage() {
+    this.$imgArray[this.index].style.display = "none";
+    if (this.index == this.imageArray.length - 1) {
+      this.index = 0;
+    } else {
+      this.index += 1;
+    }
+    this.$imgArray[this.index].style.display = "block";
+  }
 }
